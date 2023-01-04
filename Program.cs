995c558f5f10ee33c7a7f02528583e8329a3707c
ekaterina <kostcat-84@mail.ru>
2023-01-04 20:37:35 +0300

    creating a method that determines the number of characters in a string

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*написать программу, которая из имеющегося массива строк формирует массив из 
+строк длина которых меньше либо равна 3 символа. */
+
+string [] ArrayConversion (string [] array)
+{
+    string element=String.Empty;
+    int size=0;
+    for (int i=0; i<array.Length; i++)
+    {
+        element=array[i];
+        size=element.Length;
+        if (size>3)
+        {
+            array[i]="";
+        }
+
+    }
+}
