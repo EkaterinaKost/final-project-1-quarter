355cda4a4fbc6f086d61495475f2c1cf054dff32
ekaterina <kostcat-84@mail.ru>
2023-01-04 20:56:50 +0300

    replaced the ArrayConversion method with a void type method

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
 ﻿/*написать программу, которая из имеющегося массива строк формирует массив из 
 строк длина которых меньше либо равна 3 символа. */
 
-string [] ArrayConversion (string [] array)
+string[] text ={"hello", "2", "world", ";-)"};
+
+PrintArray(text);
+
+ArrayConversion(text);
+
+PrintArray(text);
+
+void ArrayConversion(string[] array)
 {
-    string element=String.Empty;
-    int size=0;
-    for (int i=0; i<array.Length; i++)
+    string element = String.Empty;
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        element=array[i];
-        size=element.Length;
-        if (size>3)
+        element = array[i];
+        size = element.Length;
+        if (size > 3)
         {
-            array[i]="";
+            array[i] = "";
         }
 
     }
+    
 }
 void PrintArray(string[] array)
 {
